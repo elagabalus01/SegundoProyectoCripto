83cb43af6c7149a92a5e3df17c5f6ee069178a9c
ApoloHuizar <carlosf.huizar@comunidad.unam.mx>
2021-12-06 20:31:16 -0600

    Front página tarjetas transacciones gobchain

--- miContrato/app/static/js/gobchain.js
+++ miContrato/app/static/js/gobchain.js
@@ -8,9 +8,27 @@ function obtener_transacciones(){
 		if(request.readyState==4 && request.status==200){
 			data=JSON.parse(request.responseText)
 			data=data["data"]
+			cont = 0
 			transacciones.innerHTML=""
 			data.forEach(item=>{
-				transacciones.innerHTML+=`<div><p>${item["Key"]}</p></div>`
+				cont += 1
+				record = item["Record"]
+				transacciones.innerHTML+=`
+    <div class="col-md-6 col-lg-4 pb-3">
+        <!-- Copy the content below until next comment-->
+        <div class="card card-custom bg-dark border-dark border-0 text-white">
+            <div class="card-custom-img" style="background-image: url(../img/fondo.jpg);"></div>
+            <div class="card-custom-avatar"><img class="img-fluid" src="../img/blockchain2.png"></div>
+            <div class="card-body" style="overflow-y: auto">
+                <h4 class="card-title">Movimiento: ${cont}</h4>
+                <p class="card-text">Registrado por: ${record["responsable"]}</p>
+                <p class="card-text">Dependencia: ${record["dependencia"]}</p>
+                <p class="card-text">Fecha: ${record["fecha"]}</p>
+                <p class="card-text">Monto: ${record["monto"]}</p>
+                <p class="card-text">Descripción: ${record["referencia"]}</p>
+            </div>
+        </div><!-- Copy until here-->
+    </div>`
 			});
 		}
 	}
