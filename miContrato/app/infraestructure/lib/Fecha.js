0cf67df3c899d56ab97991acf3f5a66e6db09c6e
Elagabalus <angelsantander609@gmail.com>
2021-12-12 21:32:45 -0600

    Correción del bug de la fecha

--- miContrato/app/infraestructure/lib/Fecha.js
+++ miContrato/app/infraestructure/lib/Fecha.js
@@ -2,7 +2,7 @@ function calcularFecha(){
     var mydate = new Date(Date.now())
     var month = ["enero", "febrero", "marzo", "abril", "mayor", "junio",
     "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"][mydate.getMonth()];
-    mydate_today=mydate.getDay()+" de "+month+" del "+mydate.getFullYear()
+    mydate_today=mydate.getDate()+" de "+month+" del "+mydate.getFullYear()
     +" a las "+mydate.getHours()+":"+mydate.getMinutes()+" hrs"
     return mydate_today
 }
